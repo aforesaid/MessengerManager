43e08eccf7e4358305793dacb7667a9d3f71c2e4
Yury <y.fomenkov@indeepa.com>
2021-09-09 02:51:44 +0300

    Add migrations for messageEntity

---
+++ src/MessengerManager.Infrastructure.Data/Migrations/20210908235119_Add_Messages.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MessengerManager.Infrastructure.Data.Migrations
+{
+    public partial class Add_Messages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Owner = table.Column<string>(type: "text", nullable: true),
+                    Text = table.Column<string>(type: "text", nullable: true),
+                    ChatThreadName = table.Column<string>(type: "text", nullable: true),
+                    Date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Sent = table.Column<bool>(type: "boolean", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Updated = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Inactive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ChatThreadName",
+                table: "Messages",
+                column: "ChatThreadName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_Date",
+                table: "Messages",
+                column: "Date");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_Owner",
+                table: "Messages",
+                column: "Owner");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_Sent",
+                table: "Messages",
+                column: "Sent");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
