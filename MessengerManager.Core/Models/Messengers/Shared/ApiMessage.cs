4fee0c1004f737f30ced710c2630fbf81e2d6bf7
Yury <y.fomenkov@indeepa.com>
2021-09-08 19:56:46 +0300

    Add message model

---
+++ MessengerManager.Core/Models/Messengers/Shared/ApiMessage.cs
@@ -0,0 +1,26 @@
+﻿namespace MessengerManager.Core.Models.Messengers.Shared
+{
+    public class ApiMessage
+    {
+        public ApiMessage()
+        { }
+
+        public ApiMessage(string text, 
+            string chatName,
+            string chatId)
+        {
+            Text = text;
+            ChatName = chatName;
+            ChatId = chatId;
+        }
+        public string Text { get; set; }
+        public string ChatName { get; set; }
+        public string ChatId { get; set; }
+
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
--- MessengerManager.Core/Services/TelegramManager/TelegramManager.cs
+++ MessengerManager.Core/Services/TelegramManager/TelegramManager.cs
@@ -1,7 +1,18 @@
-﻿namespace MessengerManager.Core.Services.TelegramManager
+﻿using Microsoft.Extensions.Logging;
+using Telegram.Bot;
+
+namespace MessengerManager.Core.Services.TelegramManager
 {
     public class TelegramManager : ITelegramManager
     {
-        
+        private readonly ITelegramBotClient _telegramBotClient;
+        private readonly ILogger<TelegramManager> _logger;
+
+        public TelegramManager(ITelegramBotClient telegramBotClient, 
+            ILogger<TelegramManager> logger)
+        {
+            _telegramBotClient = telegramBotClient;
+            _logger = logger;
+        }
     }
 }
